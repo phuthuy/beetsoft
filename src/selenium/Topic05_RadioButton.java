8576e97fba575846660d14cf08d5f3ef057a6827
thuynt <nguyenthuy35nb@gmail.com>
2018-08-07 11:07:57 +0700

    test

---
+++ src/selenium/Topic05_RadioButton.java
@@ -0,0 +1,36 @@
+package selenium;
+
+import org.testng.annotations.Test;
+
+
+
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Topic05_RadioButton {
+	WebDriver driver;
+  
+  @BeforeClass
+  public void beforeClass() {
+	  driver = new FirefoxDriver();
+	  driver.get("http://live.guru99.com/");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.manage().window().maximize();
+  }
+  @Test
+  public void TC_01() {
+	 String currURL =  driver.getCurrentUrl();
+	  Assert.assertEquals(currURL, "http://live.guru99.com/");
+  }
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
